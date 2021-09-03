fa8b7a51c6d8bf7d5240d32e41a13c51c6418727
Ashish Gupta <ashish.gupta@thepsi.com>
2021-09-03 17:40:48 +0530

    Added Extent Report support

--- src/test/java/testcases/TimeSheetTest.java
+++ src/test/java/testcases/TimeSheetTest.java
@@ -12,7 +12,7 @@ public class TimeSheetTest extends TestBase {
     @Parameters("date")
     @Test
     public void fillTimeSheet(String date) throws InterruptedException{
-        TimeSheetPage timeSheetPage = new TimeSheetPage();
-        Assert.assertTrue(timeSheetPage.iVerifyThatTimeSheetFilledSuccessMessageDisplayed(date));
+         TimeSheetPage timeSheetPage = new TimeSheetPage();
+         Assert.assertTrue(timeSheetPage.iVerifyThatTimeSheetFilledSuccessMessageDisplayed(date));
     }
 }
